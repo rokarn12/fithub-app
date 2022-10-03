20922e937c25736b0a233391ee3983f212930516
PJ <pj@pjclet.me>
2022-10-03 17:42:15 -0400

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/fithub_logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          FitHub
+        </h1>
+        <h2>
+          Expect more from your clothes.
+        </h2>
       </header>
     </div>
   );
