7a6473875f29d1e85b988b2194491547523f882c
Rojan Karn <rokarn12@gmail.com>
2022-10-05 13:10:29 -0400

    App set up with MERN
    initialized MERN app and started frontend for login/signup

---
+++ fithub_mongodb/server/app.js
@@ -0,0 +1,35 @@
+// import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+
+// app
+const app = express();
+
+
+// db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    UseUnifiedTopology: true,
+}).then(() => console.log('DB CONNECTED'))
+.catch(err => console.log('DB CONNECTION ERROR', err));
+
+
+// middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+
+
+// routes
+
+
+
+// port
+const port = process.env.PORT || 8080;
+
+// listener
+const server = app.listen(port, () => 
+    console.log('Server is running on', {port})
+);
