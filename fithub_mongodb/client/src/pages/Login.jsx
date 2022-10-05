86a8c0d073f4f9ce06e42b71bcfe9e585d84a3fc
Rojan Karn <rokarn12@gmail.com>
2022-10-05 14:52:29 -0400

    Updated login page
    added text fields and submit button

--- fithub_mongodb/client/src/pages/Login.jsx
+++ fithub_mongodb/client/src/pages/Login.jsx
@@ -14,10 +14,57 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const Login = () => {
     // form states
-    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="container mt-5 mb-5 col-10 col-sm-8 col-md-6 col-lg-5">
+            <div className="text-center mb-5 alert alert-primary">
+                <label htmlFor="" className="h2">
+                    Login
+                </label>
+            </div>
+
+            <div className="form-group">
+                <TextField 
+                    size="small"
+                    variant="outlined"
+                    className="form-control"
+                    label="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+            </div>
+
+            <div className="form-group">
+                <FormControl 
+                    variant="outlined"
+                    size="small"
+                    className="form-control"
+                    >
+                        <InputLabel>Password</InputLabel>
+                        <OutlinedInput 
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            endAdornment={
+                                <InputAdornment>
+                                    <IconButton edge="end" onClick={() => setShowPassword(!showPassword)}>
+                                        {showPassword ? (<VisibilityIcon />) : (<VisibilityOffIcon />)}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            />
+                </FormControl>
+            </div>
+
+            <div className="text-center mt-4">
+                <Button variant="contained" disabled={!email || !password}>
+                            Submit
+                </Button>
+            </div>
         </div>
     );
 };
