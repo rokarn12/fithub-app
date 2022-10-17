f1d228474b3bef4cbeacbd1d53495f920a03631a
PJ <pj@pjclet.me>
2022-10-17 16:30:25 -0400

    created home page branch

--- fithub_mongodb/client/src/pages/Home.jsx
+++ fithub_mongodb/client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import './css/home.css';
+
 const Home = () => {
     return (
-        <div className="container text-center" style={{marginTop: "12rem"}}>
-            <h1>FitHub</h1>
-            <h2>Expect more from your clothes.</h2>
+        
+        <div className="container text-center" style={{marginTop: "5rem"}}>
+            <div>
+                <h1>FitHub</h1>
+                <h2>Expect more from your clothes.</h2>
+            </div>
+            
             <div className="alert alert-primary p-5">
                 <h1>Home</h1>
             </div>
