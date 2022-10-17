fb5a11504b23bcce8b48af57022dfa258fd0bd3c
PJ <pj@pjclet.me>
2022-10-17 14:37:01 -0400

    test home page updates

--- fithub_mongodb/client/src/pages/Home.jsx
+++ fithub_mongodb/client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 const Home = () => {
     return (
         <div className="container text-center" style={{marginTop: "12rem"}}>
+            <h1>FitHub</h1>
+            <h2>Expect more from your clothes.</h2>
             <div className="alert alert-primary p-5">
                 <h1>Home</h1>
             </div>
