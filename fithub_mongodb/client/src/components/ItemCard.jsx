b832018972af03ff59a21205d415dc14b78f7ffa
PJ <pj@pjclet.me>
2022-10-20 17:28:34 -0400

    more front-end changes

--- fithub_mongodb/client/src/components/ItemCard.jsx
+++ fithub_mongodb/client/src/components/ItemCard.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 
 const itemCard = (props) => {
+    // const color1 = "red"
+    const color2 = "blue"
+    const current_color = color2
+
+    // const move = async () => {
+    //     while (true){
+            
+    //         setTimeout(function(){},1000);
+    //         current_color = color1;
+    //         setTimeout(function(){},1000);
+    //         current_color = color2;
+    //     }
+    // }
+
+    // move();
+
     return (
-        <div className='media'>
+        <div id='itemCard' className='media'>
             <img id='img1' src={props.img_src} className='mr-3' alt='img1'></img>
             <div className='media-body text-left'>
                 <h4 className='mt-0 mb-1'>{props.article}</h4>
-                <p classname='mt-0 mb-1'>Color: {props.color} <b>|</b> Type: {props.type}</p>
+                <p classname='mt-0 mb-1'>Color: {current_color} <b>|</b> Type: {props.type}</p>
                 <p>&ensp;{props.description}</p>
             </div>
         </div>
--- fithub_mongodb/client/src/pages/Home.jsx
+++ fithub_mongodb/client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import './css/home.css';
@@ -11,31 +11,37 @@ import {
 import ItemCard from './../components/ItemCard.jsx';
 
 const Home = () => {
+
     return (
         
         <div className="container text-center" style={{marginTop: "1rem"}}>
             <div id='back1' className="container mt-5 mb-5">
                 <h1>FitHub</h1>
                 <h2>Expect more from your clothes.</h2>
+                <p>Description here: lorem ipsum</p>
             </div>
-            <div className="container mt-5 mb-5 col-lg">
+            <div id='back2' className="container mt-5 mb-5 col-lg">
                 <ItemCard article='Hat' color = 'Red' type='Casual' img_src={require('./images/cap.png')}
                         description="It's looking pretty sunny out today. We want to make sure you're looking stylish and protected! It's looking pretty sunny out today. We want to make sure you're looking stylish and protected! It's looking pretty sunny out today. We want to make sure you're looking stylish and protected!"/>
                 <ItemCard article='Shirt' color = 'White' type='Casual'
-                        description="We love this T-shirt, what do you think?"/>
+                        description="We love this T-shirt, it has a light color to contrast your hat and shorts, what do you think?"/>
                 <ItemCard article='Shorts' color = 'Brown' type='Casual'
                         description="These shorts match your other clothing perfectly!"/>
                 <ItemCard article='Shoes' color = 'Grey' type='Casual'
                         description="Your schedule seems busy today, these should support you throughout the whole day."/>
             </div>            
             <div className="text-center col-lg">
-                <Button variant="contained">
-                            Sign Up
-                </Button>
+                <Link className="nav-link" to="/signup">
+                    <Button variant="contained">
+                                Sign Up
+                    </Button>
+                </Link>
                 {' '}
-                <Button variant="contained">
-                            Login
-                </Button>
+                <Link className="nav-link" to="/login">
+                    <Button variant="contained">
+                                Login
+                    </Button>
+                </Link>
             </div>
         </div>
     );
