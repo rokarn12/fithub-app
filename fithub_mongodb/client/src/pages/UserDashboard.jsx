e92329fb268bd480c3efc9d03f2551f7eecfa862
Rojan Karn <rokarn12@gmail.com>
2022-10-26 14:17:27 -0400

    minor formatting fix to UserDashboard page

--- fithub_mongodb/client/src/pages/UserDashboard.jsx
+++ fithub_mongodb/client/src/pages/UserDashboard.jsx
@@ -4,8 +4,7 @@ import { UserContext } from '../UserContext';
 const UserDashboard = () => {
     const {user} = useContext(UserContext);
     return (
-        <div className="container text-center" style={{marginTop: "12rem"}}>
-            <h1>FitHub</h1>
+        <div className="container text-center">
             <div className="alert alert-primary p-5">
                 <h1>{user && <span className='text-success'>{user}'s</span>}{" "}
                     Personal Dashboard</h1>
