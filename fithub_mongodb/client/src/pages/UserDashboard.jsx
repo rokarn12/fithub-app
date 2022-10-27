13e9659351862cb7a5ba13c7817f568f8eeed803
PJ <pj@pjclet.me>
2022-10-27 17:33:48 -0400

    added buttons for user dashboard

--- fithub_mongodb/client/src/pages/UserDashboard.jsx
+++ fithub_mongodb/client/src/pages/UserDashboard.jsx
@@ -1,6 +1,12 @@
 import React, {useContext} from 'react';
 import { UserContext } from '../UserContext';
 
+import './css/home.css';
+
+import {
+    Button
+} from '@mui/material';
+
 const UserDashboard = () => {
     const {user} = useContext(UserContext);
     return ((user ? ( // user is logged in, show their dashboard
@@ -9,6 +15,21 @@ const UserDashboard = () => {
                 <h1>{user && <span className='text-success'>{user}'s</span>}{" "}
                     Personal Dashboard</h1>
             </div>
+
+            <div className="container">
+                <div className="container">
+                    <Button id="button" variant="contained">Add Item</Button> {' '}
+                    <Button variant="contained">E-Closet</Button>{' '}
+                    <Button variant="contained">Saved Outfits</Button>{' '}
+                    {/* <br></br> */}
+                </div>
+                <div className="container">
+                    <br></br>
+                    <Button variant="contained">Generate New Outfit</Button>
+                    <br></br>
+                </div>
+            </div>
+
         </div>
         // add user dashboard functionality here
     ) : ( // user is not logged in, show this message
