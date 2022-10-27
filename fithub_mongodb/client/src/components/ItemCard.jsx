0ff2cd3a09c5697758a4a32a419bf5e150880795
PJ <pj@pjclet.me>
2022-10-27 16:35:58 -0400

    commit home screen changes

--- fithub_mongodb/client/src/components/ItemCard.jsx
+++ fithub_mongodb/client/src/components/ItemCard.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 const itemCard = (props) => {
     // const color1 = "red"
-    const color2 = "blue"
-    const current_color = color2
+    // const color2 = "blue"
+    // const current_color = color2
 
     // const move = async () => {
     //     while (true){
@@ -19,10 +19,10 @@ const itemCard = (props) => {
 
     return (
         <div id='itemCard' className='media'>
-            <img id='img1' src={props.img_src} className='mr-3' alt='img1'></img>
+            <img id='img_icon' src={props.img_src} className='mr-3' alt='img1' style={{backgroundColor: props.color}}></img>
             <div className='media-body text-left'>
                 <h4 className='mt-0 mb-1'>{props.article}</h4>
-                <p classname='mt-0 mb-1'>Color: {current_color} <b>|</b> Type: {props.type}</p>
+                <p classname='mt-0 mb-1'>Color: {props.color} <b>|</b> Type: {props.type}</p>
                 <p>&ensp;{props.description}</p>
             </div>
         </div>
