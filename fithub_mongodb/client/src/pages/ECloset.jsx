6c332823cb0f539a447671047097240d251a85ed
x-u-b <bbenx20@gmail.com>
2022-11-08 23:49:44 -0500

    sort e-closet

--- fithub_mongodb/client/src/pages/ECloset.jsx
+++ fithub_mongodb/client/src/pages/ECloset.jsx
@@ -13,11 +13,14 @@ import {
     Button
 } from '@mui/material';
 
+
 const ECloset = () => { 
 
     const {user} = useContext(UserContext);
     const navigate = useNavigate();
     const [allItems, setAllItems] = useState([]);
+    const [sortType, setSortType] = useState("Alphabetical");
+
     // var allItems = [];
 
     const handlegetItems = async (e) => {
@@ -44,6 +47,58 @@ const ECloset = () => {
     //handlegetItems();
     console.log(allItems, " <- all");
     // window.alert("hi"); handlegetItems()
+    const sortBy = async (e) => {
+        console.log("yippipippi");
+        function byAlpha( a, b ) {
+            if ( a.clothingType < b.clothingType ){
+              return -1;
+            }
+            if ( a.clothingType > b.clothingType ){
+              return 1;
+            }
+            return 0;
+          }
+          function byColor( a, b ) {
+            if ( a.color < b.color ){
+              return -1;
+            }
+            if ( a.color > b.color ){
+              return 1;
+            }
+            return 0;
+          }
+          function byAttire( a, b ) {
+            if ( a.attireType < b.attireType ){
+              return -1;
+            }
+            if ( a.attireType > b.attireType ){
+              return 1;
+            }
+            return 0;
+          }
+          e.preventDefault();
+          try {
+              console.log("1");
+              const res = await getItems(user);
+              console.log("success");
+              if (res.error) toast.error(res.error);
+              else {
+                  toast.success(res.message);
+                  // window.alert(res.message);
+                  // redirect user back to add item page
+                  navigate('/ecloset', {replace: true});
+                  //apply sorting
+                  if (sortType === "Alphabetical") setAllItems((res.items).sort(byAlpha));
+                  else if (sortType === "Color") setAllItems((res.items).sort(byColor));
+                  else if (sortType === "Attire") setAllItems((res.items).sort(byAttire));
+                  // console.log("successfully received all it")
+              }
+          } catch (err) {
+              console.log(err);
+              navigate('/userdashboard', {replace: true});
+              toast.error(err);
+          }
+    }
 
     return ((user ? ( // user is logged in, show their dashboard
         <div className="container text-center">
@@ -53,6 +108,17 @@ const ECloset = () => {
                 </div>
                 <Button variant="contained" size="large" onClick={handlegetItems} >
                     Refresh E-Closet
+                </Button>
+                <div id='sorting' className="container">
+                    <label for="sortType">Attire Type: </label>
+                    <select name="sortType" id="sort" value={sortType} onChange={(e) => setSortType(e.target.value)}>
+                    <option value="Alphabetical">Alphabetical</option>
+                    <option value="Color">Color</option>
+                    <option value="Attire">Attire Type</option>
+                    </select>
+                </div>
+                <Button variant="contained" size="large" onClick={sortBy} >
+                    Sort
                 </Button>
                 <div>
                     {allItems.map((item) => (
